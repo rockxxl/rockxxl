9ef643f3a14eed99aeda6c6a0da47a97f3411a55
Maarten Van Hoof <maartenvanhoof1989@gmail.com>
2018-12-23 21:16:35 +0100

    feat(post): added layout and container to template

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { graphql } from "gatsby";
+import Layout from "../components/Layout";
+import Container from "../components/Container";
 
 export default function Template({
     data, // this prop will be injected by the GraphQL query below.
@@ -7,10 +9,14 @@ export default function Template({
     const { markdownRemark } = data; // data.markdownRemark holds our post data
     const { frontmatter, html } = markdownRemark;
     return (
-        <article>
-            <h1>{frontmatter.title}</h1>
-            <div dangerouslySetInnerHTML={{ __html: html }} />
-        </article>
+        <Layout>
+            <Container>
+                <article>
+                    <h1>{frontmatter.title}</h1>
+                    <div dangerouslySetInnerHTML={{ __html: html }} />
+                </article>
+            </Container>
+        </Layout>
     );
 }
 
