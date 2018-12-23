42b3dfe0709be4ad6807c9f3a3dfcd928c2d1824
Maarten Van Hoof <maartenvanhoof1989@gmail.com>
2018-12-23 21:12:10 +0100

    feat(global styles): updated heading sizes

--- src/components/Layout/GlobalStyle.js
+++ src/components/Layout/GlobalStyle.js
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
-    @import url(${props => props.theme.font.family.import});
+    @import url("${props => props.theme.font.family.import}");
 
     html {
         font-size: ${props => props.theme.font.size.default}px;
@@ -20,4 +20,10 @@ export default createGlobalStyle`
         letter-spacing: 0;
         font-weight: ${props => props.theme.font.weight.headings};
     }
+
+    h1 { font-size: ${props => props.theme.font.size.xxxxl}rem; }
+    h2 { font-size: ${props => props.theme.font.size.xxxl}rem; }
+    h3 { font-size: ${props => props.theme.font.size.xxl}rem; }
+    h4 { font-size: ${props => props.theme.font.size.xl}rem; }
+    h5 { font-size: ${props => props.theme.font.size.lg}rem; }
 `;
