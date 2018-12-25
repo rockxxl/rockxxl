391caa7fd2df975ff08207ce9160c06e762243e2
Maarten Van Hoof <maartenvanhoof1989@gmail.com>
2018-12-25 22:19:43 +0100

    feat(category): updated template

--- src/templates/category.js
+++ src/templates/category.js
@@ -1,17 +1,15 @@
 /* eslint-disable react/no-danger */
 import React from "react";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
 import Layout from "../components/Layout";
 import Container from "../components/Container";
 
-export default function Template({
-    data,
-}) {
+export default function Template({ data }) {
     const { markdownRemark } = data;
     const {
-        frontmatter: {
-            title,
-        }, html,
+        frontmatter: { title },
+        fields: { posts },
+        html,
     } = markdownRemark;
     return (
         <Layout>
@@ -20,18 +18,42 @@ export default function Template({
                     <h1>{title}</h1>
                     <div dangerouslySetInnerHTML={{ __html: html }} />
                 </article>
+                <ul>
+                    {posts ? posts.map(({
+                        fields: { slug },
+                        frontmatter: { title: postTitle },
+                    }) => (
+                        <li key={postTitle}>
+                            <Link to={slug}>{ postTitle }</Link>
+                        </li>
+                    )) : null}
+                </ul>
+
             </Container>
         </Layout>
     );
 }
 
 export const pageQuery = graphql`
-  query($path: String!) {
-    markdownRemark(fields: { slug: { eq: $path } }) {
-      html
-      frontmatter {
-        title
-      }
+    query($path: String!) {
+        markdownRemark(fields: { slug: { eq: $path } }) {
+            fileAbsolutePath
+            html
+            fields {
+                slug
+                posts {
+                    html
+                    fields {
+                        slug
+                    }
+                    frontmatter {
+                        title
+                    }
+                }
+            }
+            frontmatter {
+                title
+            }
+        }
     }
-  }
 `;
